bd80f3d2c27499be92d42f85f3d16edca7522faf
bdmediapro <90436267+bdmediapro@users.noreply.github.com>
2026-02-11 21:34:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -79,9 +79,11 @@ app.post("/chat", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 app.get("/start", (req, res) => {
   res.send("Use POST to start a session.");
