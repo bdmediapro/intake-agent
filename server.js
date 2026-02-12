636c671ef4a2c6696f9d4b8afcef48886c2ea496
bdmediapro <90436267+bdmediapro@users.noreply.github.com>
2026-02-11 21:31:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const cors = require("cors");
-
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const OpenAI = require("openai");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 const openai = new OpenAI({
